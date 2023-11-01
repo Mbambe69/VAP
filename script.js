2a6e11838c609ab6fef4d9dd300883bf39b2d0e5
2.E Dohnal Richard <Richard.Dohnal@student.sps-cl.cz>
2023-11-01 13:29:42 +0100

    First init

--- script.js
+++
@@ -1,8 +0,0 @@
-function fibCislo (n){
-   if (n<2) return 
-    return fibCislo(n-1)+ fibCislo(n-2);
-   
-}
-function vypocitej (){
-    alert(fibCislo(Number(document.getElementById("vstup").value)))
-}
